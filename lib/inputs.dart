1b86cbbbf75b0515752efca2b855a0745c6a74ea
seaso <seasonmaharjan@kathmandu.codes>
2022-11-28 10:24:50 +0545

    .

--- lib/inputs.dart
+++ lib/inputs.dart
@@ -1,17 +1,19 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 
 class AppInputField extends StatelessWidget {
 
   final String hinttext;
-   // ignore: use_key_in_widget_constructors
-   const AppInputField(this.hinttext);
+  final TextEditingController controller;
+  const AppInputField({Key? key, required this.hinttext, required this.controller}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
       child: TextField(
+        controller: controller,
         decoration: InputDecoration(
           contentPadding:
           const EdgeInsets.symmetric(vertical: 12, horizontal: 22),
--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:news/custombutton.dart';
+import 'package:news/auth.dart';
 import 'package:news/dashboard.dart';
-import 'package:news/inputs.dart';
 
 class SplashPage extends StatefulWidget{
   const SplashPage({Key? key}) : super(key: key);
@@ -34,92 +33,100 @@ class Signin extends StatefulWidget{
 
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return SigninState();
   }
 
 }
 class SigninState extends State<Signin>{
+  String? errorMessage = '';
+  bool isLogin = true;
+
+  final TextEditingController controllerEmail = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+  Future<void> signInWithEmailAndPassword() async {
+    try {
+      await Auth().signInWithEmailAndPassword(
+        email: controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      await Auth().createUserWithEmailAndPassword(
+        email: controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+  Widget _errorMessage() {
+    return Text(errorMessage == '' ? '' : 'Humm ? $errorMessage');
+  }
+
+  Widget _submitButton() {
+    return ElevatedButton(
+      onPressed:
+      isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
+      child: Text(isLogin ? 'Login' : 'Register'),
+    );
+  }
+
+  Widget _loginOrRegisterButton() {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          isLogin = !isLogin;
+        });
+      },
+      child: Text(isLogin ? 'Register instead' : 'Login instead'),
+    );
+  }
+  Widget _title() {
+    return const Text('Firebase Auth');
+  }
+  Widget _entryField(
+      String title,
+      TextEditingController controller,
+      ) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: title,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-   return Scaffold(
-     backgroundColor:Colors.white,
-     body: SingleChildScrollView(
-       child: SizedBox(
-         height: size.height,
-         width: size.width,
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             Container(
-               padding: const EdgeInsets.only(bottom: 25.0),
-               color: Colors.white,
-               child: Center(
-                   child: Image.asset('images/logo.png', width: 180,)
-               ),
-             ),
-             Container(
-               margin: const EdgeInsets.all(20.0),
-               padding: const EdgeInsets.all(25.0),
-               height: size.height/1.7,
-               width: double.infinity,
-               decoration: const BoxDecoration(
-                   borderRadius: BorderRadius.all( Radius.circular(30),),
-                   color: Color(0xffFBAF43)
-               ),
-               child: Column(
-                 children: [
-                   Container(
-                     width: double.infinity,
-                     padding: const EdgeInsets.symmetric(vertical: 8.0),
-                     child: const Text(
-                       'Sign In',
-                       style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),
-                     ),
-                   ),
-                   const AppInputField("Email"),
-                   const AppInputField("Password"),
-                   OrangeButton(
-                     title: 'Sign In', onTap: (){
-
-                     Navigator.push(context,
-                         MaterialPageRoute(builder: (context)=>
-                         Dashboard()));
-                   },
-                   ),
-                   Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: [
-
-                       GestureDetector(
-                         onTap: ()=> {},
-                             // Navigator.push(context,
-                             // MaterialPageRoute(builder: (context)=>
-                             // const WebView())),
-                         child: const Padding(
-                           padding: EdgeInsets.all(5.0),
-                           child: Text('SignUp', style: TextStyle(fontSize: 12,fontWeight: FontWeight.normal),
-                           ),
-
-                         ),
-                       ),
-                       Text(' | ', style: Theme.of(context).textTheme.bodyText1),
-                       const Padding(
-                         padding: EdgeInsets.all(5.0),
-                         child: Text('Forgot Password?', style: TextStyle(fontSize: 12,fontWeight: FontWeight.normal),),
-                       ),
-
-
-                     ],
-                   )
-                 ],
-               ),
-             )
-           ],
-         ),
-       ),
-     ),
-   );
+   // var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: _title(),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _entryField('email', controllerEmail),
+            _entryField('password', _controllerPassword),
+            _errorMessage(),
+            _submitButton(),
+            _loginOrRegisterButton(),
+          ],
+        ),
+      ),
+    );
   }
 }
